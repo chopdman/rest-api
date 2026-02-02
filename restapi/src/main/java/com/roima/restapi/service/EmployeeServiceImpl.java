b3c1408f97e12dae67bd1a29ee97126ec31da68d
chopdman <man.chopda@roimaint.com>
2026-02-02 22:36:10 +0530

    fix dependency bug

--- restapi/src/main/java/com/roima/restapi/service/EmployeeServiceImpl.java
+++ restapi/src/main/java/com/roima/restapi/service/EmployeeServiceImpl.java
@@ -4,10 +4,10 @@ package com.roima.restapi.service;
 import com.roima.restapi.dto.request.EmployeeRequestDto;
 import com.roima.restapi.dto.response.EmployeeResponseDto;
 import com.roima.restapi.entity.Employee;
+import com.roima.restapi.exception.ResourceNotFoundException;
 import com.roima.restapi.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
