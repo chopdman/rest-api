f003f5598608389fd4d0399a3558abce850299e9
chopdman <man.chopda@roimaint.com>
2026-02-03 10:24:19 +0530

    add jwt secret into properties

--- restapi/src/main/java/com/roima/restapi/security/JwtUtil.java
+++ restapi/src/main/java/com/roima/restapi/security/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -13,7 +14,8 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private static final String SECRET = "why_should_itell_y0u_mann_09alaod";
+    @Value("${jwt.secret}")
+    private String SECRET;
 
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
